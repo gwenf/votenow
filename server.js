417e32644a26d7b9cd49d0d14e4835d6d83b2e67
gwenf <gwenfaraday@gmail.com>
2015-10-19 22:24:39 -0400

    building login and register views

--- server.js
+++ server.js
@@ -6,19 +6,35 @@ var bodyParser = require("body-parser");
 var path = require('path');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/votedb');
+//mongoose.connect('mongodb://localhost/votedb');
 
 app.set('view engine', 'jade');
 app.set('views', path.resolve(__dirname,'client','views'));
 
-app.use(express.static(path.resolve(__dirname, 'client')));
+//app.use(express.static(path.resolve(__dirname, 'client')));
 
 //app.use(bodyParser.urlencoded({extended: true}));
 //app.use(bodyParser.json());
 //app.get("/api/v1",require("./routes/api"));
 
-router.get('/', function (req, res) {
-  res.render('index');
+app.get('/', function (req, res) {
+  res.render('index', {});
+});
+
+app.get('/register', function(req, res){
+	res.render('register');
+});
+
+app.get('/login', function(req, res){
+	res.render('login');
+});
+
+app.get('/user-page', function(req, res){
+	res.render('user-page');
+});
+
+app.get('/logout', function(req, res){
+	res.redirect('/');
 });
 
 app.listen(3000, function(){
