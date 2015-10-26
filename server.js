45b7f2178d769d32bb7cecc0dcb253ca2b4606d3
gwenf <gwenfaraday@gmail.com>
2015-10-25 22:59:25 -0400

    added bcrypt and client-sessions for authentication

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
+var sessions = require('client-sessions');//mozilla library for cookies, etc...
 
 var path = require('path');
 var mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://localhost/votedb');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var User = mongoose.model('user', new Schema({
+	id: ObjectId,
+	firstName: String,
+	lastName: String,
+	email: {type: String, unique: true},
+	password: String
+}));
+
+//database
+mongoose.connect('mongodb://localhost/votenow');
 
 app.set('view engine', 'jade');
 app.set('views', path.resolve(__dirname,'client','views'));
@@ -16,8 +29,13 @@ app.locals.pretty = true;
 
 //middleware - allows access to the body of the http request
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(bodyParser.json());
-//app.get("/api/v1",require("./routes/api"));
+
+app.use(sessions({
+	cookieName: 'session',
+	secret: 'sjfiej8u382hjfj89jfwieds21jksd',
+	duration: 30 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000
+}));
 
 app.get('/', function (req, res) {
   res.render('index', {});
@@ -27,19 +45,71 @@ app.get('/register', function(req, res){
 	res.render('register');
 });
 
+//app.post('/register', function(req, res){
+//	res.json(req.body);
+//});
+
+//post handler for register.jade
 app.post('/register', function(req, res){
-	res.json(req.body);
+	var user = new User({
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: req.body.password
+		});
+	user.save(function(err){
+		if(err){
+			var error = 'something bad happened :(';
+			if(err===11000){
+				error='That email is already taken. Please try another one.';
+			}
+			res.render('register', {error: error});
+		} else {
+			//if user registration (.save function) works they should be directed to their user page
+			res.redirect('userPage');
+		}
+		
+	});
 });
 
 app.get('/login', function(req, res){
 	res.render('login');
 });
 
-app.get('/user-page', function(req, res){
-	res.render('user-page');
+app.post('/login', function(req, res){
+	User.findOne({ email: req.body.email }, function(err,user){
+		if (!user){
+			res.render('login', {error: 'Invalid email or password.'});
+		} else {
+			if (req.body.password === user.password){
+				req.session.user = user; //set-cookie: session=...
+				res.redirect('userPage');
+			} else {
+				res.render('login', {error: 'Invalid email or password.'});
+			}
+		}
+	});
+});
+
+app.get('/userPage', function(req, res){
+	if (req.session && req.session.user){
+		User.findOne({ email: req.session.user.email }, function(err, user){
+			if (!user){
+		console.log('problem found');
+				req.session.reset();
+				res.redirect('/login');
+			} else {
+				res.locals.user = user; //to access in templates
+				res.render('userPage');
+			}
+		});
+	} else {
+			res.redirect('login');
+	}
 });
 
 app.get('/logout', function(req, res){
+	req.session.reset();
 	res.redirect('/');
 });
 
