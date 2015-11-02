2d1e6a0108fe185a7d4565b02e96d89162c75d90
gwenf <gwenfaraday@gmail.com>
2015-11-02 00:09:50 -0500

    adding angular to create posts

--- client/js/app.js
+++ client/js/app.js
@@ -1,2 +1,21 @@
-var myApp = angular.module('myApp',['ngRoute']);
+$(document).ready(function(){
 
+    var myApp = angular.module('myApp', ['ngRoute']);
+
+    myApp.controller('ChoiceCtrl', function ($scope) {
+        $scope.choices = [];
+
+        $scope.addChoice = function () {
+            if ($scope.choiceBody) {
+                $scope.choices.shift({
+                    body: $scope.choiceBody
+                });
+                $scope.choiceBody = null;
+            }
+        }
+    });
+
+    //$('.love').append('<p>what up</p>');
+
+
+});
